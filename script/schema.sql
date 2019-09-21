a3cdac7b82f9d613a7dc6a05e2a4753f8dc0e277
Rutger Vos <rutgeraldo@gmail.com>
2019-09-21 12:48:21 +0200

    updated schema

--- script/schema.sql
+++ script/schema.sql
@@ -1,23 +1,22 @@
 CREATE TABLE refsites(
+    refsite_id integer not null constraint refsite_pk primary key,
     chromosome integer not null,
-	position integer not null,
-	refallele integer default 0,
-	primary key (chromosome,position)
+	position integer not null
 );
 
 CREATE TABLE accessions(
-	accession text constraint code_pk primary key,
-	variety text,
-	category text,
-	origin text,
-	latitude real,
-	longitude real
+    accession_id integer not null constraint accession_pk primary key,
+    label text,
+    botanical_variety text,
+    status text,
+    country text,
+    latitude real,
+    longitude real
 );
 
 CREATE TABLE snps(
-	chromosome integer not null constraint chromosome_fk references refsites(chromosome),
-	position integer not null constraint position_fk references refsites(position),
-	altallele integer not null,
-	accession text not null constraint accession_fk references refsites(accession),
-	primary key (chromosome,position,accession)
+    snp_id integer not null constraint snp_pk primary key,
+    refsite_id integer not null constraint refsite_fk references refsites(refsite_id),
+    accession_id integer not null constraint accession_fk references accessions(accession_id),
+	altallele integer not null
 );	
