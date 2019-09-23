5b4d1f437b5072b6d7de8edde5023fc165e1f428
Rutger Vos <rutgeraldo@gmail.com>
2019-09-23 14:11:42 +0200

    making new database

---
+++ script/gff2csv_bases.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+# transforms GFF columns into CSV for the SNP table in the database
+while(<>) {
+	chomp;
+	s/SL2\.50ch0*//;
+	my ( $chromo, $pos, $id, $ref, $alt ) = split /\t/, $_;
+	my ( $hom, $het ) = split /,/, $alt;
+	print join(',', $chromo, $pos, $ref, $hom, $het), "\n";
+}
+
--- script/schema_bases.sql
+++ script/schema_bases.sql
@@ -1,9 +1,10 @@
--- this schema encodes SNPs as 0 (ref), 1 (het), 2 (homo alt)
+-- this schema encodes SNPs as [ACGT] (ref), [ACGT] (het), [ACGT] (homo alt)
 
 CREATE TABLE refsites(
     refsite_id integer not null constraint refsite_pk primary key,
     chromosome integer not null,
-	position integer not null
+	position integer not null,
+	refallele text not null
 );
 
 CREATE TABLE accessions(
@@ -20,5 +21,6 @@ CREATE TABLE snps(
     snp_id integer not null constraint snp_pk primary key,
     refsite_id integer not null constraint refsite_fk references refsites(refsite_id),
     accession_id integer not null constraint accession_fk references accessions(accession_id),
-	altallele integer not null
-);	
+	altallele_hom text not null,
+	altallele_het text not null
+);
