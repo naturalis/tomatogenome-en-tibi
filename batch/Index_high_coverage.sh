e964efb764bc72aa7d0e45def1adbcb7f10027a4
ewout130cc <59561910+ewout130cc@users.noreply.github.com>
2020-02-10 14:11:47 +0100

    Added batch scripts for the rest of the pipeline

---
+++ batch/Index_high_coverage.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+#Given paired_En-Tibi.fixmate.sorted.markdup.depth, creates a 2-column TSV (chromo,pos) with sites cover>=10,
+#i.e. mincover10.tsv, which is used by bcftools.
+#SBATCH --job-name=index_coverage
+#SBATCH --output=output_Index_high_coverage.txt
+
+while(<>) {
+	chomp;
+	@f = split /\t/, $_;
+	last if $f[0] !~ /^\d+$/;
+	if ( $f[2] >= 10 ) {
+		print sprintf('SL2.50ch%02d', $f[0]), "\t", $f[1], "\n";
+	}
