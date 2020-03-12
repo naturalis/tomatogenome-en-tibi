c390c8b86de5e918f5bb2ab0c72240ab252af17c
ewout130cc <ewout.130cc@gmail.com>
2020-03-12 14:36:37 +0000

    Changed filenames and bugfixes

--- batch/Index_high_coverage.sh
+++ batch/Index_high_coverage.sh
@@ -9,5 +9,7 @@ while(<>) {
 	@f = split /\t/, $_;
 	last if $f[0] !~ /^\d+$/;
 	if ( $f[2] >= 10 ) {
-		print sprintf('SL2.50ch%02d', $f[0]), "\t", $f[1], "\n";
+#		print sprintf('SL2.50ch%02d', $f[0]), "\t", $f[1], "\n";
+                printf("%d\t%d\n", $f[0], $f[1]);
 	}
+}
