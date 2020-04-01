bcbf5ce5d7d2280288f8a1c48568b052def9b4fd
ewout130cc <59561910+ewout130cc@users.noreply.github.com>
2020-04-01 13:15:46 +0200

    Added cutadapt command to Fastp.sh

--- batch/Fastp.sh
+++ batch/Fastp.sh
@@ -7,7 +7,15 @@ echo "Quality assessment and trimming"
 fastp \
         -i "/home/rutger.vos/fileserver/projects/B19015-525/updated/run0220_En-Tibi_S2_L003_R1_001.nophix.fastq.gz" \
         -I "/home/rutger.vos/fileserver/projects/B19015-525/updated/run0220_En-Tibi_S2_L003_R2_001.nophix.fastq.gz" \
-        -o "paired_R1_fastp.fastq.gz" \
-        -O "paired_R2_fastp.fastq.gz" \
+        -o "untrimmed_paired_R1_fastp.fastq.gz" \
+        -O "untrimmed_paired_R2_fastp.fastq.gz" \
         -j fastp.json -h fastp.html --verbose
 
+echo "Cutadapt"
+
+cutadapt \
+        -a ADAPTER_FWD -A ADAPTER_REV \
+        -o paired_R1_fastp.fastq.gz \
+        -p paired_R2_fastp.fastq.gz \
+        untrimmed_paired_R1_fastp.fastq.gz \
+        untrimmed_paired_R2_fastp.fastq.gz
